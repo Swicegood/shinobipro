5e21e8ec0e690a8831328592784f80a76e86e1fe
Jagadguru <jguru108@gmail.com>
2022-03-05 22:03:30 -0500

    revert start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,4 @@ set -e
 pm2-docker /opt/shinobi/pm2Shinobi.yml &
 sleep 15
 
-### Check if face-plugin installed
-if [ ! -f /.installed ]; then
-  echo "New install detected - please wait while we face-plugin - will take up to 30 minutes!"
-  /usr/bin/yes no | /opt/shinobi/plugins/face/INSTALL.sh
-
-  cp /opt/shinobi/plugins/face/conf.json.real /opt/shinobi/plugins/face/conf.json
-  touch /.installed    
-fi  
-
 pm2-docker /opt/shinobi/face.yml
